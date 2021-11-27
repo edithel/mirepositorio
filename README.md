9bccbe94e2bebdb8e742e6bc0408b10183ebf852
edithel <edith65elisa@gmail.com>
2021-11-27 15:05:09 -0300

    se agrego comentario y readme

---
+++ README.md
@@ -0,0 +1,3 @@
+#Este es un repositorio de pruebe de Edith
+aca va la descripcion
+este es el primer commit
--- helloword.py
+++ helloword.py
@@ -1,4 +1,8 @@
+#git status ===> esto te muestra el estado actual de tu proyecto con los cambios si es que hubo
+#git add =======> esto aniade al stage area tu cambio
+#git commit -m "mensaje"==> esta es la descripcion que va a tener tu commit o tu cambio
+
 # este mensaje imprime código
-print("Hello")
 print("segundo print")
+print("Hello")
 edith="soy edith"
